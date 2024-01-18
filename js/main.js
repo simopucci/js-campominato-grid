99e233c31a70b5450c4fe6116b721e720d50fbd4
simopucci <simo.pucci7@gmail.com>
2024-01-18 17:23:48 +0100

    feat: add js for difficult 1

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// # FUNCTIONS
+
+function gameGrid(container) {
+    container.innerHTML = '';
+
+    for (let i = 1; i <= 100; i++) {
+        const boxElement = gameBox(i);
+        container.append(boxElement);
+    }
+}
+
+function gameBox(text) {
+    const box = document.createElement('div');
+    box.classList.add('box-100');
+    box.innerText = text;
+
+    box.addEventListener('click', function() {
+        box.classList.toggle('clicked');
+        console.log(box.innerText);
+    })
+
+    return box;
+}
+
+// # END FUNCTIONS
+
+const gameBoard = document.getElementById('gameboard');
+const playButton = document.getElementById('play-button');
+
+gameGrid(gameBoard);
+
+playButton.addEventListener('click', function() {
+    gameGrid(gameBoard);
+})
